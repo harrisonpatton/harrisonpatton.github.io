e1c26781f1169a21791592eb88426aaf25e0f3b3
Harry Belden <harry.belden@gmail.com>
2017-12-12 15:15:32 -0600

    added new headshots and rotating about images

---
+++ script.js
@@ -0,0 +1,11 @@
+var image = document.getElementById("headshot");
+var curr = 0;
+var headshots = ["images/belden_headshot1.jpg", "images/belden_headshot2.jpg", "images/belden_headshot3.jpg", "images/belden_headshot4.jpg", "images/belden_headshot5.jpg", "images/belden_headshot6.jpg", "images/belden_jewel.png"];
+
+function changepic(){
+  var image = document.getElementById("headshot");
+  var index = Math.floor(Math.random() * 5)+0;
+  image.src= headshots[index];
+}
+
+setInterval(changepic, 4000);
