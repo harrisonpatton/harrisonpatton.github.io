e59ece163eb95717d5fdfa4ce2ad039896601fa8
Harry Belden <harry.belden@gmail.com>
2017-12-12 15:21:48 -0600

    tests

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var headshots = ["images/belden_headshot1.jpg", "images/belden_headshot2.jpg", "
 function changepic(){
   var image = document.getElementById("headshot");
   var index = Math.floor(Math.random() * 5)+0;
+  console.log(index);
+  console.log(headshots[index]);
   image.src= headshots[index];
 }
 
